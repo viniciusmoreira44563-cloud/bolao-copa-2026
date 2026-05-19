e176d5abdf06c59819c5214d2cbc80f87a039866
viniciusmoreira44563-cloud <viniciusmoreira44563@gmail.com>
2026-05-19 14:47:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -662,4 +662,5 @@ def api_avatar():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+   port = int(os.environ.get("PORT", 5001))
+app.run(host="0.0.0.0", port=port, debug=True)
